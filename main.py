b570a252a399a6ce944629a3fa740dcf652957db
gucio321 <gucio321@protonmail.com>
2024-12-16 13:57:18 +0100

    check first, then generate code

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from barcode.writer import SVGWriter
 
 if __name__ == '__main__':
     num = input("Podaj numer: ")
-    with open("code.svg", 'wb') as f:
-        ITF(str(num), writer=SVGWriter()).write(f)
     if len(str(num)) > 16:
         raise ValueError("Max number length is 16 digits. See https://pl.wikipedia.org/wiki/Przeplatany_2_z_5 ")
+    with open("code.svg", 'wb') as f:
+        ITF(str(num), writer=SVGWriter()).write(f)
     print("Kod zapisany w pliku code.svg")
