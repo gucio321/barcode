6010a2024496cee58f2dc08b079ab5424c944eda
gucio321 <gucio321@protonmail.com>
2024-12-16 13:56:04 +0100

    now its 10 lines

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 from barcode import ITF
 from barcode.writer import SVGWriter
 
-
-class Barcode:
-    def __init__(self, number : int):
-        """
-        :param number: barcode number that will be encoded.
-        """
-        # validation
-        if len(str(number)) > 16:
-            raise ValueError("Max number length is 16 digits. See https://pl.wikipedia.org/wiki/Przeplatany_2_z_5 ")
-        self.number = number
-
-    def generate(self):
-        with open("code.svg", 'wb') as f:
-            ITF(str(self.number), writer=SVGWriter()).write(f)
-
-def main():
-    try:
-        num = int(input("Podaj numer: "))
-    except ValueError:
-        print("Nieprawidłowy numer")
-    Barcode(num).generate()
-    print("Kod zapisany w pliku code.svg")
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    num = input("Podaj numer: ")
+    with open("code.svg", 'wb') as f:
+        ITF(str(num), writer=SVGWriter()).write(f)
+    if len(str(num)) > 16:
+        raise ValueError("Max number length is 16 digits. See https://pl.wikipedia.org/wiki/Przeplatany_2_z_5 ")
+    print("Kod zapisany w pliku code.svg")
